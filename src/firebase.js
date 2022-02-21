532a046030e90ad88d9814fe1cbd668a519f8097
Prati <pratimeshshinde121@gmail.com>
2022-02-21 14:30:54 +0530

    firebase old

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import firebase from 'firebase'
 
-const firebaseConfig = {
+  const firebaseConfig = {
     apiKey: "AIzaSyAu6VZ6qxMCnipGSKJ5eMiReXjwVz0vGa8",
     authDomain: "tesla-web-clone-87fb1.firebaseapp.com",
     projectId: "tesla-web-clone-87fb1",
     storageBucket: "tesla-web-clone-87fb1.appspot.com",
     messagingSenderId: "785521001929",
     appId: "1:785521001929:web:5b8146c19ec872f0cdd418"
-  };
-
-  const firebaseApp = initializeApp(firebaseConfig)
-
-  const auth = getAuth();
-
-  onAuthStateChanged(auth, user => { console.log(user) })
-
+  }
+  
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  
+  const auth = firebaseApp.auth()
+  
   export { auth }
